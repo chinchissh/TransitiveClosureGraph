86a9191a71650855ca1e1ce48cc0f028894f08cb
Чинчи Саны-Шири <Sany-ShiriCE@mer.ci.nsu.ru>
2023-05-29 11:16:44 +0700

    Добавьте файлы проекта.

---
+++ TransitiveClosureGraph/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TransitiveClosureGraph
+{
+    public class Program // Графы, транзитивное замыкание
+    {
+        public static void Main()
+        {
+            int[,] adjacencyMatrix = {
+            { 0, 3, 0, 2, 0, 0, 0, 0 },
+            { 0, 0, 5, 0, 4, 0, 0, 0 },
+            { 0, 5, 0, 0, 0, 1, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 6 },
+            { 0, 0, 0, 0, 0, 0, 0, 7 },
+            { 0, 0, 0, 0, 0, 0, 3, 0 },
+            { 0, 0, 0, 0, 0, 3, 0, 2 },
+            { 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+            TransitiveClosure transitiveClosure = new TransitiveClosure(adjacencyMatrix);
+            int[,] closure = transitiveClosure.GetTransitiveClosure();
+
+            Console.WriteLine("Транзитивное замыкание графа:");
+            for (int i = 0; i < closure.GetLength(0); i++)
+            {
+                for (int j = 0; j < closure.GetLength(1); j++)
+                {
+                    Console.Write(closure[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TransitiveClosureGraph/TransitiveClosure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitiveClosureGraph
+{
+    public class TransitiveClosure
+    {
+        private int[,] closure;
+        private int verticesCount;
+
+        public TransitiveClosure(int[,] adjacencyMatrix)
+        {
+            verticesCount = adjacencyMatrix.GetLength(0);
+            closure = new int[verticesCount, verticesCount];
+
+            // Инициализация транзитивного замыкания исходной матрицей смежности
+            Array.Copy(adjacencyMatrix, closure, verticesCount * verticesCount);
+        }
+
+        public int[,] GetTransitiveClosure()
+        {
+            for (int k = 0; k < verticesCount; k++)
+            {
+                for (int i = 0; i < verticesCount; i++)
+                {
+                    for (int j = 0; j < verticesCount; j++)
+                    {
+                        closure[i, j] = @closure[i, j] !=
+                            0 || (closure[i, k] != 0 && closure
+                            [k, j] != 0) ? 1 : 0;
+                    }
+                }
+            }
+
+            return closure;
+        }
+    }
+}
